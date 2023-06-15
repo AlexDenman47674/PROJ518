2a67cb5f5e9c0a10fe5a3632496193bd008e1a84
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2023-06-15 13:04:10 +0100

    Implemented the Integer Version 1 Algorithm

--- Lehmer_Generator/Lehmer_Generator_Implementation/Lehmer_Generator_Implementation/Program.cs
+++ Lehmer_Generator/Lehmer_Generator_Implementation/Lehmer_Generator_Implementation/Program.cs
@@ -8,8 +8,50 @@ namespace Lehmer_Generator_Implementation
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //Park and Miller's implementation of the Lehmer Generator or minimal standard generator consists of 4 algorithms
+            //Two algorithms dedicated to an integer based generator
+            //And another two dedicated to a real number based generator
+
+            //Declares the value to be given
+            List<double> ReturnValues = new List<double>();
+
+            //Integer Version 1
+            IntegerVer1(ref ReturnValues);
+            for (int i = 0; i <= ReturnValues.Count()-1; i++)
+            {
+                Console.WriteLine(ReturnValues[i]);
+            }
+            Console.ReadLine();
+        }
+
+        public static void IntegerVer1(ref List<double>Values)
+        {
+            //Integer Versions of the algorithm use an integer based seed instead of a double or float
+            //This algorithm will produce values between 1 and -1
+
+            //Empties the list before generation
+            Values.Clear();
+
+            //Declares the seed multiplier (A) and the modifier (M) as integers
+            //For the algorithms used the original 1988 value of A has been replaced by Park and Miller's updated value to ensure greater accuracy
+            const int A = 48271;
+            const int M = 214783647;
+
+            //The base seed used for all algorithms featured will remain the same
+            //The specific base seed used for testing is the authors date of birth (30/10/2000) as it provided a value large enough for authentic generation
+            int seed = 30102000;
+
+            //100 Generations are stored within ReturnValues
+            for (int i = 1; i <= 100; i++)
+            {
+                seed = (A * seed) % M;
+
+                //Since an int seed is used, conversion must take place otherwise all decimals are truncated to 0 before storage
+                Values.Add((Convert.ToDouble(seed) / M));
+            }
         }
     }
 }
