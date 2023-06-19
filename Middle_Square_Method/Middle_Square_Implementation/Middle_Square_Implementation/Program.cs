a8d1c83ebd1ceda55a7f6876dd71d5968ee3ac60
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2023-06-19 13:21:54 +0100

    Created Middle Square Implementation

---
+++ Middle_Square_Method/Middle_Square_Implementation/Middle_Square_Implementation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middle_Square_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
