b287a7373fd0e5f714b5e83d07101127dc788c0c
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2023-07-26 13:12:16 +0100

    Update RAnalysis.R

--- Data_Analysis/RAnalysis.R
+++ Data_Analysis/RAnalysis.R
@@ -7,6 +7,26 @@ library(RDieHarder)
 library(rjson)
 
 #The working directory is set to allow access to stored JSON files
-setwd("D:/Github/PROJ518")
+setwd("D:/Github/PROJ518/C#_Rand_Function/RandFunctionOutput")
 
-result <- fromJSON(file = "input.json")
+#Retrieving the Dice Sim data from the C# output
+CDiceSim1Values <- fromJSON(file = "C#DiceSim1.json")
+CDiceSim2Values <- fromJSON(file = "C#DiceSim2.json")
+
+print(CDiceSim1Values)
+print(CDiceSim2Values)
+
+#Retrieving the Dice Sim data from the Python output
+setwd("D:/Github/PROJ518/Python_Rand_Function/Python_Output")
+PDiceSim1Values <- fromJSON(file = "PythonDiceOutput1.json")
+PDiceSim2Values <- fromJSON(file = "PythonDiceOutput2.json")
+PDiceSim3Values <- fromJSON(file = "PythonDiceOutput3.json")
+
+print(PDiceSim1Values)
+print(PDiceSim2Values)
+print(PDiceSim3Values)
+
+#Retrieving the Dice Sim data from the JavaScript output
+setwd("D:/Github/PROJ518/Javascript_Rand/Javascript_Rand_Output")
+JSDiceSim1Values <- fromJSON(file = "JavaScriptDiceSim.json")
+print(JSDiceSim1Values)
