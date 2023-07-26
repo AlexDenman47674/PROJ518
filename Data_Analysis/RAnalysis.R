28314c1adb270c7611c9d85afb827de302e99c6e
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2023-07-26 12:35:50 +0100

    Imported working dependencies

--- Data_Analysis/RAnalysis.R
+++ Data_Analysis/RAnalysis.R
@@ -1,3 +1,12 @@
+#Dependencies need to be installed before analysis can begin
+#RDieharder contains the test suite that will be used and RJson allows for the reading of JSON files
 install.packages("RDieHarder")
+install.packages("rjson")
 
 library(RDieHarder)
+library(rjson)
+
+#The working directory is set to allow access to stored JSON files
+setwd("D:/Github/PROJ518")
+
+result <- fromJSON(file = "input.json")
